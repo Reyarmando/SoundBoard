aae4fc249c991b4662612820bb1f9bbd61731b75
Reyarmando <reyarmando.jimenez@tecsup.edu.pe>
2021-06-29 20:55:18 -0700

    proyecto completo de reproduccion en tableview y eliminacion

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,109 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by Reyarmando Jimenez on 29/06/21.
+//
+
+import UIKit
+import AVFoundation
+
+class SoundViewController: UIViewController{
+
+    @IBOutlet weak var grabarButton: UIButton!
+    @IBOutlet weak var nombreTextField: UITextField!
+    @IBOutlet weak var reproducirButton: UIButton!
+    
+    @IBOutlet weak var agregarButton: UIButton!
+    
+    @IBOutlet var tablaGrabaciones: UIView!
+    var grabarAudio:AVAudioRecorder?
+    var reproducirAudio:AVAudioPlayer?
+    var audioURL:URL?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configuracionGrabacion()
+        
+        reproducirButton.isEnabled = false
+        agregarButton.isEnabled = false
+        // Do any additional setup after loading the view.
+    }
+    
+    func configuracionGrabacion(){
+        do{
+            //creando sesion de audio
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: [])
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            // creando direccion para el archivo
+            let basePath:String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath,"audio.m4a"]
+            audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+    
+            //imprim ruta dnd se guarda arch
+            print("***********************")
+            print(audioURL!)
+            print("***********************")
+            
+            //crear opciones prra el grabador de audio
+            var settings:[String:AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
+            settings[AVSampleRateKey] = 44100.0 as AnyObject?
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject?
+            
+            //Crear el objeto de grab d aud
+            grabarAudio = try AVAudioRecorder(url: audioURL!, settings: settings)
+            grabarAudio!.prepareToRecord()
+            
+        }catch let error as NSError{
+            print(error)
+        }
+    }
+    
+    @IBAction func grabarTapped(_ sender: Any) {
+        if grabarAudio!.isRecording{
+            //detener grabacion
+            grabarAudio?.stop()
+            //cambiar txt del boton grb
+            grabarButton.setTitle("GRABAR", for: .normal)
+            reproducirButton.isEnabled = true
+            agregarButton.isEnabled = true
+        }else{
+            //empezar a grabar
+            grabarAudio?.record()
+                //cambiar txt btn grb a dtnr
+            grabarButton.setTitle("DETENER", for: .normal)
+            reproducirButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func reproducirTapped(_ sender: Any) {
+        do{
+            try reproducirAudio = AVAudioPlayer(contentsOf: audioURL!)
+            reproducirAudio!.play()
+            print("Reproduciendo")
+        }catch{}
+    }
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let grabacion = Grabacion(context: context)
+        grabacion.nombre = nombreTextField.text
+        grabacion.audio = NSData(contentsOf: audioURL!)! as Data
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
